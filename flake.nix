3d8b4a6a6809dea60bee8281bfc14479330cb72d
Carmine Paolino <carmine@paolino.me>
2026-09-24 10:08:02 +0200

    Prepare Spotifast 0.10.0

--- flake.nix
+++ flake.nix
@@ -120,7 +120,7 @@
                 pname = "spotifast";
                 version = (pkgs.lib.importTOML ./Cargo.toml).package.version;
                 src = self;
-                hash = "sha256-D5aqvw8sNeNr3sc2Md/HBxsbc2dVu2pLAUJeBSLv0uw=";
+                hash = "sha256-GM3UA5JwWD2Xx79NNrCSzBUnHW4fcWmvtMsOXQBs+BY=";
               };
               # projectm-sys only searches lib, while CMake may otherwise install to lib64.
               postPatch = ''
